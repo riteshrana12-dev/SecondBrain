7b1d18f1ccff509cebfcf2103af66580ce465413
riteshrana12-dev <ranaritesh677@gmail.com>
2026-05-01 00:38:48 +0530

    feat(content): enable content update with tag reprocessing
    - Added updateContent controller to modify title, link, type, and tags
    - Reused hybrid tag logic to support both existing tag IDs and new tag names
    - Ensured updated content returns latest document with populated tag references
    - Added error handling for missing content and server failures

--- backend/src/controllers/content.controller.ts
+++ backend/src/controllers/content.controller.ts
@@ -62,4 +62,53 @@ const deleteContent = async (req: Request, res: Response) => {
   }
 };
 
-export default { addContent, deleteContent };
+const updateContent = async (req: Request, res: Response) => {
+  try {
+    const { contentId } = req.params;
+    const { title, link, type, tags } = req.body;
+
+    let tagIds: Types.ObjectId[] = [];
+    for (let tag of tags) {
+      if (Types.ObjectId.isValid(tag)) {
+        // selected tag from frontedn
+        tagIds.push(new Types.ObjectId(tag));
+      } else {
+        // new tag created
+        let existing = await tagModel.findOne({ tag });
+        if (!existing) {
+          existing = await tagModel.create({ tag });
+        }
+        tagIds.push(existing._id);
+      }
+    }
+
+    const updated = await contentModel.findByIdAndUpdate(
+      contentId,
+      {
+        title,
+        link,
+        type,
+        tags: tagIds,
+      },
+      { new: true },
+    );
+
+    if (!updated) {
+      return res.status(404).json({
+        success: false,
+        message: "Content not found",
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      message: "Content updated",
+      updated,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      error,
+    });
+  }
+};
+export default { addContent, deleteContent, updateContent };
