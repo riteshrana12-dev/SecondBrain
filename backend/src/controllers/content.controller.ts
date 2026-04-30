ac9aa3d5bc790569820ceff2c9dff95b698c10e4
riteshrana12-dev <ranaritesh677@gmail.com>
2026-05-01 00:37:05 +0530

    feat(content): implement content creation with dynamic tag handling
    - Added addContent controller to create new content items
    - Integrated hybrid tag logic: reuse existing tag IDs or create new tags by name
    - Ensured tags array always stores valid ObjectId references
    - Attached userId from auth middleware for ownership tracking

---
+++ backend/src/controllers/content.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import contentModel from "../models/content.model";
+import tagModel from "../models/tag.model";
+import { Types } from "mongoose";
+
+const addContent = async (req: Request, res: Response) => {
+  try {
+    const { title, link, type, tags } = req.body;
+    const userId = req.user_id;
+
+    let tagIds: Types.ObjectId[] = [];
+    for (let tag of tags) {
+      if (Types.ObjectId.isValid(tag)) {
+        // selected tag from frontedn
+        tagIds.push(new Types.ObjectId(tag));
+      } else {
+        // new tag created
+        let existing = await tagModel.findOne({ tag });
+        if (!existing) {
+          existing = await tagModel.create({ tag });
+        }
+        tagIds.push(existing._id);
+      }
+    }
+
+    const Content = await contentModel.create({
+      title,
+      link,
+      type,
+      userId,
+      tags: tagIds,
+    });
+
+    return res.status(200).json({
+      success: true,
+      Content,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "server error",
+      error,
+    });
+  }
+};
+
+export default { addContent };
