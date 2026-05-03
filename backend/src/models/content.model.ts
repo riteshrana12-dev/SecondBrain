afa31a4f1762d4fc39c0f9a3d577254d0bb86fae
riteshrana12-dev <ranaritesh677@gmail.com>
2026-05-03 23:56:56 +0530

    feat(content): support file uploads with fileUrl in schema

--- backend/src/models/content.model.ts
+++ backend/src/models/content.model.ts
@@ -2,20 +2,30 @@ import { Schema, model, Document, Types } from "mongoose";
 
 interface IContent extends Document {
   title: string;
-  link: string;
+  link?: string;
   type: string;
+  notes: string;
+  fileUrl?: string;
+  extractedText?: string;
+  embedding?: number[];
+  isEmbedded?: boolean;
   userId: Types.ObjectId;
   tags: Types.ObjectId[];
 }
 
 const contentSchema = new Schema<IContent>({
   title: { type: String, required: true },
-  link: { type: String, required: true },
+  link: { type: String },
   type: {
     type: String,
-    enum: ["document", "video", "image", "link"],
+    enum: ["document", "youtube", "tweet", "link", "post"],
     required: true,
   },
+  notes: { type: String, dafault: "" },
+  fileUrl: { type: String, default: "" },
+  extractedText: { type: String, default: "" },
+  embedding: { type: [Number], default: [] },
+  isEmbedded: { type: Boolean, default: false },
   userId: { type: Types.ObjectId, ref: "user", required: true },
   tags: [{ type: Types.ObjectId, ref: "tag" }],
 });
