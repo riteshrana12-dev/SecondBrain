70ccb1265bfff3b73f7de7d3104395e21dc192fa
riteshrana12-dev <ranaritesh677@gmail.com>
2026-05-04 15:22:53 +0530

    feat(home): set up Home component with core UI imports

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useState, useMemo } from "react";
+import Sidebar from "../components/ui/Sidebar";
+import Card from "../components/ui/Card";
+import Modal from "../components/ui/Modal";
+import SearchBar from "../components/ui/SearchBar";
